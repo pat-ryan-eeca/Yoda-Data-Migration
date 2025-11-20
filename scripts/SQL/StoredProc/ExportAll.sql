43fe7879666a46d7088078a726d29b2135296e9a
pat-ryan-eeca <pat.ryan@eeca.govt.nz>
2025-11-20 13:49:15 +1300

    added prject code to projects

--- scripts/SQL/StoredProc/ExportAll.sql
+++ scripts/SQL/StoredProc/ExportAll.sql
@@ -1,6 +1,6 @@
 USE [GEM_UAT]
 GO
-/****** Object:  StoredProcedure [dbo].[ExportAll]    Script Date: 11/20/2025 10:50:46 AM ******/
+/****** Object:  StoredProcedure [dbo].[ExportAll]    Script Date: 11/20/2025 1:43:52 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -13,6 +13,7 @@ GO
 CREATE OR ALTER      PROCEDURE [dbo].[ExportAll] 
 	@ProgramId INT,
 	@SubProgramId INT,
+	@External_Reference VARCHAR(200)='',
 	@Project_Code VARCHAR(200)='',
 	@RootPath VARCHAR(255) = 'c:\temp\GEMEXPORT2\',
 	@SkipFiles BIT = 1  -- if 1 (true) files are not exported
@@ -49,7 +50,7 @@ EXEC xp_cmdshell  @cmd
 
 --GetGEMSubProgramProjects
 SET @header =  'PRINT(''Program_ID,Subprogram_ID,Project_ID, Applicant, Contract_Title, Reference, Status, Start_Date, End_Date, EECA_Contact, Round, project_Last_Updated_On, contract_Last_Updated_On'');' 
-SET @sql = 'EXEC GetGEMSubProgramProjects ' +  CONVERT(VARCHAR(10), @ProgramId) +',' +   CONVERT(VARCHAR(10), @SubProgramId)  +'"';
+SET @sql = 'EXEC GetGEMSubProgramProjects ' +  CONVERT(VARCHAR(10), @ProgramId) +',' +   CONVERT(VARCHAR(10), @SubProgramId)  +  +',' +   CONVERT(VARCHAR(100), @External_Reference) + ',''' +  CONVERT(VARCHAR(100), @Project_Code) +'''"';
 SET @cmd = 'sqlcmd -S EECAGEMUDB1 -d GEM_UAT -Q "'  + @header + @sql + ' -o ' + @RootPath + '\GEMSubProgramProjects.csv -s "," -h -1 -y ' + CONVERT(VARCHAR(10), @MaxFieldLength);
 select @cmd
 EXEC xp_cmdshell  @cmd
@@ -85,7 +86,7 @@ EXEC xp_cmdshell  @cmd
 
 --GetGEMSubProgramContractVariations		 
 SET @header =  'PRINT(''project_id, Contract_ID, Applicant, Workflow_Instance_ID, Title,  Type, RequestedOn, ContractorSignatoryName,status,Request_Summary,InternalReviewComment,InternalReviewRecommendation,ContractDescription, CompletionDate, CommencementDate, ContractorSignatoryPosition,ContractCap,AuthorityToSign,ProjectCode, File_ID, File_Name'');' 
-SET @sql = 'EXEC GetGEMSubProgramContractVariations ' +  CONVERT(VARCHAR(10), @ProgramId) +',' +   CONVERT(VARCHAR(10), @SubProgramId) + +',' +   CONVERT(VARCHAR(10), @Project_Code) + '"';
+SET @sql = 'EXEC GetGEMSubProgramContractVariations ' +  CONVERT(VARCHAR(10), @ProgramId) +',' +   CONVERT(VARCHAR(10), @SubProgramId) +',' +   CONVERT(VARCHAR(10), @Project_Code) + '"';
 SET @cmd = 'sqlcmd -S EECAGEMUDB1 -d GEM_UAT -Q "' + @header + @sql + ' -o ' + @RootPath + '\GEMSubProgramContractVariations.csv -s "," -h -1 -y ' + CONVERT(VARCHAR(10), @MaxFieldLength);
 select @cmd
 EXEC xp_cmdshell  @cmd
--- scripts/SQL/StoredProc/GetGEMSubProgramProjects.sql
+++ scripts/SQL/StoredProc/GetGEMSubProgramProjects.sql
@@ -1,6 +1,6 @@
 USE [GEM_UAT]
 GO
-/****** Object:  StoredProcedure [dbo].[GetGEMSubProgramContracts]    Script Date: 11/17/2025 10:40:40 AM ******/
+/****** Object:  StoredProcedure [dbo].[GetGEMSubProgramProjects]    Script Date: 11/20/2025 1:48:06 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
@@ -10,27 +10,36 @@ GO
 -- Create date: <Create Date,,>
 -- Description:	<Description,,>
 -- =============================================
-CREATE OR ALTER PROCEDURE [dbo].[GetGEMSubProgramProjects]
+ALTER   PROCEDURE [dbo].[GetGEMSubProgramProjects]
 	-- Add the parameters for the stored procedure here
 	@ProgramId INT,
 	@SubProgramId INT,
-	@External_Reference VARCHAR(200)=''
+	@External_Reference VARCHAR(200)='',
+	@Project_Code VARCHAR(200)=''
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-  
-select @ProgramId,@SubProgramId,  p.Project_ID as Project_ID, c.Stakeholder_id as applicant,  p.Project_Title as Contract_Title, p.External_Reference, 
-		p.Project_Status_ID, p.Project_Actual_Start_Date, p.Project_Actual_Finish_Date,pd.value as EECA_CONTACT, t.Name as Round
+
+with t1 as (
+select @ProgramId as Program, @SubProgramId as SubProgram,  p.Project_ID as Project_ID, c.Stakeholder_id as Applicant,  p.Project_Title as Contract_Title, p.External_Reference, 
+		p.Project_Status_ID as Project_Status_ID, p.Project_Actual_Start_Date , p.Project_Actual_Finish_Date,pd.value as EECA_CONTACT, t.Name as Round, dbo.CleanString(CAST((gd.Value) AS VARCHAR(MAX))) as Project_Code
 		from Contract c join Project p  on c.Project_ID=p.Project_ID
 		inner join STKH_Stakeholders s on c.Stakeholder_ID = s.stakeholder_id
 		inner join STKH_Legal l on l.stakeholder_id=s.stakeholder_id
 		inner join SEC_StakeholderRoles sr on s.stakeholder_id = sr.stakeholder_id and sr.universal_resource_id=p.Uro_ID
 		inner join PRJ_ProjectData pd on pd.universal_resource_id=p.Uro_ID
 		inner join TimePeriod t on t.Time_Period_ID = p.Time_Period_ID
-
+		INNER JOIN  dbo.GrantWorkflowInstance gwi on gwi.Grant_ID=p.Project_ID and p.Subprogram_ID =@SubProgramId
+		
+		--project code is defined in a child workflow of the contract variation workflow
+		INNER join GrantWorkflowInstance gwic on gwic.Parent_Workflow_Instance_ID = gwi.Workflow_Instance_ID
+		LEFT JOIN dbo.GrantData gd on
+			gd.Workflow_Instance_ID = gwic.Workflow_Instance_ID                 
+			and gd.Field_Definition_ID=1700
+			and gd.Grant_ID = gwic.Grant_ID
 
 		where p.Subprogram_ID =@SubProgramId 
 		and p.Program_ID = @ProgramId 
@@ -38,5 +47,28 @@ select @ProgramId,@SubProgramId,  p.Project_ID as Project_ID, c.Stakeholder_id a
 		and  p.Project_Actual_Start_Date is not null
 		and pd.field_code='PROJ_EECA_CONTACT'
 		and p.External_Reference like '%'+@External_Reference +'%'
+		and CAST(gd.Value AS VARCHAR(MAX)) like  '%'+@Project_Code +'%'
+ 
+ )
+
+ -- dedupe
+	select distinct 
+		Program,
+		SubProgram,
+		Project_ID,
+		Applicant,
+		Contract_Title,
+		External_Reference,
+		Project_Status_ID,
+		t1.Project_Actual_Start_Date , 
+		t1.Project_Actual_Finish_Date,
+		EECA_CONTACT,
+		Round,
+		Project_Code
+
+
+
+		from t1
+
  
 END
