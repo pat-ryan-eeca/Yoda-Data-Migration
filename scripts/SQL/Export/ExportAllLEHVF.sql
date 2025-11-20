e851f6e64817e72b0f8bbe1a2c7f5366f8db3bd5
pat-ryan-eeca <pat.ryan@eeca.govt.nz>
2025-11-20 16:26:26 +1300

    added project code

--- scripts/SQL/Export/ExportAllLEHVF.sql
+++ scripts/SQL/Export/ExportAllLEHVF.sql
@@ -1,6 +1,7 @@
 DECLARE @ProgramId INT= 10;
 DECLARE @SubProgramId INT =42;
 DECLARE @External_Ref VARCHAR(200)='';
+DECLARE @Project_Code VARCHAR(200)='';
 DECLARE @RootPath VARCHAR(255) = 'c:\temp\GEMEXPORT2\LEHVF\'
 
-EXEC  ExportAll @ProgramID, @SubProgramId, @External_Ref, @RootPath, 1
+EXEC  ExportAll @ProgramID, @SubProgramId, @External_Ref,@Project_Code, @RootPath, 1
